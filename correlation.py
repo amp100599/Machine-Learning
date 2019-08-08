114f5016a61e9932abaad29899d59279adf1e1dd
amp100599 <53916157+amp100599@users.noreply.github.com>
2019-08-09 00:48:42 +0530

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -6,7 +6,7 @@ Created on Thu Aug  1 23:38:58 2019
 """
 
 import pandas as pd
-from tkinter import Tk,Label
+#from tkinter import Tk,Label
 
 
 phy_marks=[15,12,8,8,7,7,7,6,5,3]
@@ -16,10 +16,10 @@ x=pd.Series(phy_marks)
 y=pd.Series(his_marks)
 
 r=x.cov(y)/(x.std()*y.std())
-root=Tk()
+#root=Tk()
 #root.title()
-root.geometry("300x300")
-l1=Label(root,text=round(r,3),font='Times 45')
-l1.pack()    
+#root.geometry("300x300")
+#l1=Label(root,text=round(r,3),font='Times 45')
+#l1.pack()    
 
-#print("Correlation coefficient : %.3f" %r)
+print("Correlation coefficient : %.3f" %r)
